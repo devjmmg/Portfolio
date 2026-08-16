517b844e92c270aeefae1cef256b72ea37bac77e
devjmmg <166524587+devjmmg@users.noreply.github.com>
2026-08-15 18:47:41 -0600

    feat: complete Home hero section

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
     return (
         <>
             <Header />
-            <main id="home" className="max-w-7xl mx-auto p-4">
+            <main id="home" className="max-w-7xl mx-auto p-4 scroll-mt-16">
                 <Home />
             </main>
-            <section id="about" className="max-w-7xl mx-auto p-4">
+            <section id="about" className="max-w-7xl mx-auto p-4 scroll-mt-16">
                 <About />
             </section>
         </>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center min-h-[calc(100vh-60px)]">
-            <div className="text-center order-2 md:order-1 space-y-6">
+            <div className="text-center order-2 md:order-1 space-y-7">
                 <p className="text-2xl font-medium text-gray-500">Hola, Soy</p>
                 <h2 className="text-5xl font-bold bg-linear-to-r from-indigo-500 to-indigo-950 bg-clip-text text-transparent">Juan Manuel Martínez García</h2>
                 <p className="text-2xl font-semibold text-gray-500">Desarrollador Full Stack</p>
@@ -23,7 +23,8 @@ export default function Home() {
                     >
                         Descargar CV
                     </a>
-                    <button
+                    <a
+                        href="#projects"
                         className="
                             px-6 py-3
                             rounded-md
@@ -33,11 +34,43 @@ export default function Home() {
                             font-medium
                             hover:border-indigo-500
                             hover:text-indigo-500
-                            transition-colors duration-300 ease-linear cursor-pointer
+                            transition-colors duration-300 ease-linear
                         "
                     >
-                        Contacto
-                    </button>
+                        Ver proyectos
+                    </a>
+                </div>
+                <div className="flex gap-4 justify-center">
+                    <a 
+                        href="tel:+522223511153"
+                        aria-label="Llamar por teléfono"
+                        className="cursor-pointer"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                        </svg>
+                    </a>
+                    <a 
+                        href="#contact"
+                        aria-label="Enviar correo"
+                        className="cursor-pointer"
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                        </svg>
+                    </a>
+                    <a
+                        href="https://github.com/devjmmg"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        aria-label="Enlace a GitHub"
+                    >
+                        <img
+                            src="/img/icons/github.svg"
+                            alt="GitHub.svg"
+                            className="size-6"
+                        />
+                    </a>
                 </div>
             </div>
             <div className="relative order-1 md:order-2">
